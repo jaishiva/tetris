c92b835a99fc88b5e289cc7202bbf7c09d32208d
jai shiva <jai.shiva4@gmail.com>
2020-04-03 12:26:19 +0530

    define all shapes and define draw method

--- main.py
+++ main.py
@@ -11,7 +11,15 @@ class shape():
                 np.ones((2,3),dtype = int)
             ],
             'S' : [
-                
+                np.ones((5,2),dtype = int),
+                np.ones((5,2),dtype = int)
+            ],
+            'C' : [
+                np.ones((2,2),dtype = int)
+            ],
+            '|' : [
+                np.ones((4,1),dtype = int),
+                np.ones((1,4),dtype = int)
             ]
         }
 
@@ -19,17 +27,24 @@ class shape():
         # use 1's to represent shape
         self.shapes['L'][0][0][1] = 0
         self.shapes['L'][0][1][1] = 0
-
         self.shapes['L'][1][1][1] = 0
         self.shapes['L'][1][1][2] = 0
-
         self.shapes['L'][2][1][0] = 0
         self.shapes['L'][2][2][0] = 0
-
         self.shapes['L'][3][0][0] = 0
         self.shapes['L'][3][0][1] = 0
 
+        self.shapes['S'][0][1][1] = 0
+        self.shapes['S'][0][3][0] = 0
+        self.shapes['S'][1][1][0] = 0
+        self.shapes['S'][1][3][1] = 0
+    
+    def draw(self,shp):
+        for i in self.shapes[shp]:
+            print(i)
+
 tetris = shape()
 
-for i in range(4):
-    print(tetris.shapes['L'][i])
+tetris.draw('L')
+tetris.draw('C')
+tetris.draw('|')
